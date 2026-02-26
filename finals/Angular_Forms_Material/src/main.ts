d58a0c57d7a847ff4cdfc7e81914894a42a71c10
Satwinder Jeerh <senka09120829@gmail.com>
2026-02-26 09:51:35 +0800

    Delete finals/Angular_Forms_Material directory

--- finals/Angular_Forms_Material/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes'; // <-- Correct path
-
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
-})
-.catch(err => console.error(err));
